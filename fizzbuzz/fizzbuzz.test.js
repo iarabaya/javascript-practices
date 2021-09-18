a234f5afb0d8234336a19a881fc9bdb6269435be
iara baya <iarabayav@gmail.com>
2021-09-18 05:26:08 -0300

    fizzbuzz with jest testing

---
+++ fizzbuzz/fizzbuzz.test.js
@@ -0,0 +1,47 @@
+const fizzbuzz = require('./fizzbuzz');
+
+//test driven development
+describe('fizzbuzz', ()=>{
+
+    test('should print an error message if the argument is not a number',()=>{
+        const expected = 'Error: the argument must be a number';
+        const result = fizzbuzz("16");
+        expect(expected).toBe(result);
+    })
+
+    test('should print 1 if receive 1', ()=>{
+        const expected = 1;
+        const result = fizzbuzz(1);
+        expect(expected).toBe(result);
+    })
+
+    test('should print fizz if receive 3', ()=>{
+        const expected = 'fizz';
+        const result = fizzbuzz(3);
+        expect(expected).toBe(result);
+    })
+
+    test('should print fizz if receive a multiple of 3', ()=>{
+        const expected = 'fizz';
+        const result = fizzbuzz(6);
+        expect(expected).toBe(result);
+    })
+    
+    test('should print buzz if receive 5', ()=>{
+        const expected = 'buzz';
+        const result = fizzbuzz(5);
+        expect(expected).toBe(result);
+    })
+
+    test('should print buzz if receive a multiple of 5', ()=>{
+        const expected = 'buzz';
+        const result = fizzbuzz(10);
+        expect(expected).toBe(result);
+    })
+    
+    test('should print fizzbuzz if receive a multiple of 3 and 5', ()=>{
+        const expected = 'fizzbuzz';
+        const result = fizzbuzz(15);
+        expect(expected).toBe(result);
+    })
+})
